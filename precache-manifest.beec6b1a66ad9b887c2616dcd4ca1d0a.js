e7f458fd49819a986eaa2bd591887c1f308559d6
Anne Thor <anne.thor@gmail.com>
2020-06-28 18:27:01 -0700

    Updates

---
+++ precache-manifest.beec6b1a66ad9b887c2616dcd4ca1d0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af06db6371e76993f6c72af9fdf7e318",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "0e883e54dfb4ea4766d6",
+    "url": "/tic-tac-toe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "476ee52475be72888084",
+    "url": "/tic-tac-toe/static/css/main.1f5a3af2.chunk.css"
+  },
+  {
+    "revision": "0e883e54dfb4ea4766d6",
+    "url": "/tic-tac-toe/static/js/2.e49a155a.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/tic-tac-toe/static/js/2.e49a155a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "476ee52475be72888084",
+    "url": "/tic-tac-toe/static/js/main.bced3d48.chunk.js"
+  },
+  {
+    "revision": "ecbc6f5089b996663329",
+    "url": "/tic-tac-toe/static/js/runtime-main.a982f7cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
